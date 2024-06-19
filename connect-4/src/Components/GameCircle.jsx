d36a78bc211a8399778140d1673e8dc621fbef26
Keamybams <rulibams16@gmail.com>
2024-06-19 10:02:11 +0100

    style

--- connect-4/src/Components/GameCircle.jsx
+++ connect-4/src/Components/GameCircle.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const onClick = (id, color) => {
+import './Game.css'
+
+const onClick = (id) => {
 
     alert("on click " + id);
 }
 
-const GameCircle = ({id, color, children}) => {
-  console.log(id);
+const GameCircle = ({id, children}) => {
+  
   return (
-    <div style={{ backgroundColor: color, width: 100, height: 100, margin: 10, borderRadius: '50%' }} onClick={() => onClick(id)}>
+    <div className={`gameCircle ${id % 2 === 0 ? "odd" : "even"}`} onClick={() => onClick(id)}>
         {children}
     </div>
   )
